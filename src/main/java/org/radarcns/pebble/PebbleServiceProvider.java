8c763e93946156abf0e55d7fa593c24dbbef36a8
Pavel Mikhailovskii <pavel@thehyve.nl>
2017-11-03 14:46:29 +0100

    Fixed PebbleServiceProvider

--- src/main/java/org/radarcns/pebble/PebbleServiceProvider.java
+++ src/main/java/org/radarcns/pebble/PebbleServiceProvider.java
@@ -44,7 +44,7 @@ public class PebbleServiceProvider extends DeviceServiceProvider<PebbleDeviceSta
 
     @SuppressWarnings("unchecked")
     public void showDetailView() {
-        new PebbleHeartbeatToast(getRadarService(), getConnection()).execute();
+        new PebbleHeartbeatToast(getConnection()).execute();
     }
 
     @Override
